09a90fefd7fd60d7dce7951cf8587173967672af
user_12_19P1 <muhametova.alcu@example.com>
2023-03-29 12:11:01 +0500

    что-то новое

--- Write_Erase/ViewModels/mBasketViewModel.cs
+++ Write_Erase/ViewModels/mBasketViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using Write_Erase.Models;
 using Write_Erase.Services;
 
@@ -28,6 +29,7 @@ namespace Write_Erase.ViewModels
 
         public DelegateCommand IncreaseNumber => new(() =>
         {
+            
             if (SelectedItem != null)
             {
                 ProductCard selectedProductCard = ProductsInBasket.Products.Where(i => i == SelectedItem).FirstOrDefault();
